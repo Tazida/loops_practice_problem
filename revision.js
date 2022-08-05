d4079f47c711f903c1fc28beb09938f1843b6ba1
Tazida <tazidarahman5@gmail.com>
2022-08-05 12:54:20 +0600

    first commit

---
+++ revision.js
@@ -0,0 +1,22 @@
+// string type variable
+var bottle = "water";
+var bottlecolor = "yellow";
+// number type variable
+var sunglassPrice = 120;
+var penColor = "blue";
+//____?____ type variable
+var inExpensive = true;
+
+var tableItems = ["bottle", "pen", "mouse", "sunglass", "paper"];
+
+var items = tableItems[3];
+tableItems[4] = "notebook";
+var penIndex = tableItems.indexOf("pen");
+// push pop
+
+// conditional
+
+if (tableItems.length > 4) {
+} else if (tableItems[3] == "pen") {
+} else {
+}
---
+++ while.js
@@ -0,0 +1,17 @@
+// if(condition){
+
+// }
+/**
+ *  1. loop variable
+ *  2. condition inside while
+ *  3. loop body
+ *  4. change the loop variable
+ */
+var roastGiven = 0;
+while (roastGiven < 7) {
+  console.log("Roast den, please !!");
+  //roastGiven = roastGiven + 1;
+  //roastGiven += 1;
+  console.log(roastGiven);
+  roastGiven++;
+}
