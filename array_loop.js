05a1af9b953be6ffa8853e49809d215db3e392b7
Tazida <tazidarahman5@gmail.com>
2022-08-05 23:53:00 +0600

    display every elements of an array

---
+++ array_loop.js
@@ -0,0 +1,16 @@
+/* for (var i = 0; i < 10; i++) {
+  console.log(i);
+}
+ */
+// display every elements of an array
+/* var numbers = [45, 87, 89, 56, 32, 25, 188, 41, 25, 98];
+for (var i = 0; i < numbers.length; i++) {
+  var number = numbers[i];
+  console.log(number);
+} */
+
+var items = ["bottle", "mouse", "sunglass", "pen"];
+for (var i = 0; i < items.length; i++) {
+  var item = items[i];
+  console.log(item);
+}
